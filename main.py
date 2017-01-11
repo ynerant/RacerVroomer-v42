3c984d3156e1e1f7646242cfb610b4e8cd9acd0b
BatMulet <maximetilliet@gmail.com>
2017-01-11 17:25:53 +0100

    42

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ def main():
 	screenHeight = window.winfo_screenheight()
 	window.geometry(str(width) + "x" + str(height) + "+" + str(int((screenWidth - width) / 2)) + "+" + str(int((screenHeight - height) / 2)))
 	
-	singleplayer = tkinter.Button(window, text="Un joueur", font=("Plantagenet Cherokee", 42), anchor="center", width=20)
-	multiplayer = tkinter.Button(window, text="Multijoueur", font=("Plantagenet Cherokee", 42), anchor="center", width=20)
+	singleplayer = tkinter.Button(window, text="Single Player", font=("Plantagenet Cherokee", 42), anchor="center", width=20)
+	multiplayer = tkinter.Button(window, text="Multiplayer", font=("Plantagenet Cherokee", 42), anchor="center", width=20)
 	options = tkinter.Button(window, text="Options", font=("Plantagenet Cherokee", 42), anchor="center", width=20)
-	quit = tkinter.Button(window, text="Quitter", font=("Plantagenet Cherokee", 42), anchor="center", width=20)
+	quit = tkinter.Button(window, text="(Don't) Quit", font=("Plantagenet Cherokee", 42), anchor="center", width=20)
 	
 	singleplayer.pack()
 	multiplayer.pack()
