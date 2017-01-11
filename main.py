69e41fc750e3a8333ad6b0bd2acd7bc8219e34e5
galaxyoyo <yohann.danello@gmail.com>
2017-01-11 16:48:07 +0100

    Hello world!

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+import tkinter
+
+
 def main():
-	print("Hello world!")
+	window = tkinter.Tk()
+	window.title("Racer Vrommer v42")
+	window.state("zoomed")
+	width = 840
+	height = 420
+	screenWidth = window.winfo_screenwidth()
+	screenHeight = window.winfo_screenheight()
+	window.geometry(str(width) + "x" + str(height) + "+" + str(int((screenWidth - width) / 2)) + "+" + str(int((screenHeight - height) / 2)))
+	
+	label = tkinter.Label(window, text="42", font="\"Plantagenet Cherokee\" 42")
+	label.pack()
+	
+	window.mainloop()
+	
+	print(42)
 
 if __name__ == "__main__":
 	main()
