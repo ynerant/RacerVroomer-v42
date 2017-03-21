c2e3552a8470e1637fd940be8ae0af8407a82599
galaxyoyo <yohann.danello@gmail.com>
2017-03-21 16:06:34 +0100

    Map chooser

--- guis/gameBuilder.py
+++ guis/gameBuilder.py
@@ -3,7 +3,7 @@
 import tkinter as tk
 import messages as msgs
 from guis import GUI, back
-import cars
+import cars, maps
 
 class GameBuilder:
 	CURRENT_GAME_BUILDER = None
@@ -54,10 +54,10 @@ class CarChooser(GUI):
 		window.columnconfigure(0, weight = 3)
 		window.columnconfigure(1, weight = 2)
 		window.columnconfigure(2, weight = 2)
-		window.columnconfigure(2, weight = 1)
+		window.columnconfigure(3, weight = 1)
 
 		backBtn = tk.Button(window, textvariable = msgs.BACK, font = ("Plantagenet Cherokee", 30), anchor = "center", width = 20, borderwidth = 10, relief = "groove", command = lambda : back(window))
-		backBtn.grid(row = len(cars.CARS) * 2 + 1, column = 0, columnspan = 3, pady = 100)
+		backBtn.grid(row = len(cars.CARS) * 2 + 1, column = 0, columnspan = 4, pady = 100)
 		self.children.append(backBtn)
 
 	def registerChooseListener(self, window, button, car):
@@ -67,11 +67,48 @@ class CarChooser(GUI):
 		button.bind("<ButtonRelease-1>", lambda event : choose_car())
 
 class MapChooser(GUI):
-	def __init__(self, window, gameBuilder : GameBuilder):
+	def __init__(self, window : tk.Tk, gameBuilder : GameBuilder):
 		GUI.__init__(self)
 
 		self.builder = gameBuilder
 
+		for i in range(len(maps.MAPS)):
+			map = maps.MAPS[i]
+			thumbnail = tk.PhotoImage(file = "images/thumbnails/" + map.thumbnail)
+			thumbnailLabel = tk.Label(window, image = thumbnail)
+			thumbnailLabel.image = thumbnail
+			thumbnailLabel.grid(row = i, column = 0, rowspan = 1)
+			label = tk.Label(window, textvariable = map.name, font = ("Plantagenet Cherokee", 30))
+			label.grid(row = i, column = 1, rowspan = 1)
+			sizeLabel = tk.Label(window, textvariable = msgs.SIZE.format(map.width, map.height), font = ("Plantagenet Cherokee", 22))
+			sizeLabel.grid(row = i, column = 2)
+			choose = tk.Button(window, textvariable = msgs.CHOOSE, font = ("Plantagenet Cherokee", 22))
+			choose.grid(row = i, column = 3, rowspan = 1)
+
+			if self.builder.map == map:
+				ok = tk.Label(window, text = "ü", font = ("Wingdings", 42))
+				ok.grid(row = i, column = 4, rowspan = 1)
+				self.children.append(ok)
+
+			self.registerChooseListener(window, choose, map)
+
+			self.children.append(thumbnailLabel)
+			self.children.append(label)
+			self.children.append(sizeLabel)
+			self.children.append(choose)
+
+		window.columnconfigure(0, weight = 1)
+		window.columnconfigure(1, weight = 3)
+		window.columnconfigure(2, weight = 2)
+		window.columnconfigure(3, weight = 2)
+		window.columnconfigure(4, weight = 1)
+
 		backBtn = tk.Button(window, textvariable = msgs.BACK, font = ("Plantagenet Cherokee", 30), anchor = "center", width = 20, borderwidth = 10, relief = "groove", command = lambda : back(window))
-		backBtn.pack()
+		backBtn.grid(row = len(cars.CARS) * 2 + 1, column = 0, columnspan = 5, pady = 100)
 		self.children.append(backBtn)
+
+	def registerChooseListener(self, window, button, map):
+		def choose_map():
+			self.builder.map = map
+			back(window)
+		button.bind("<ButtonRelease-1>", lambda event : choose_map())
---
+++ maps.py
@@ -0,0 +1,49 @@
+#coding : utf-8
+
+import messages as msgs
+import json
+#import tkinter as tk
+
+MAPS = []
+
+class Map :
+	def __init__(self, name : msgs.MSG, fileName):
+		self.name = name
+		self.fileName = fileName
+
+		try:
+			with open("maps/" + fileName, "r") as f:
+				jsoned = f.read()
+		except:
+			print("An error occured while loading the map called \"" + name.en + "\" with filename \"" + fileName + "\", map not loaded")
+			return
+
+		obj = json.loads(jsoned) #type: dict
+
+		self.img_file = obj["img_file"]
+		self.thumbnail = obj["thumbnail"]
+		self.width = int(obj["width"])
+		self.height = int(obj["height"])
+		self.start = Wall(obj["start"])
+		raw_walls = obj["walls"]
+		self.walls = []
+		for raw_wall in raw_walls:
+			self.walls.append(Wall(raw_wall))
+
+		MAPS.append(self)
+
+	def __str__(self):
+		return "{Map name=\"" + self.name.get() + "\", size=" + str(self.width) + "x" + str(self.height) + ", filename=\"" + self.fileName + "\", img_file=\"" + self.img_file\
+			   + "\", start=" + str(self.start) + ", walls=" + str(self.walls) + "}"
+
+class Wall :
+	def __init__(self, args):
+		self.x_start = int(args[0])
+		self.x_end = int(args[1])
+		self.y_start = int(args[2])
+		self.y_end = int(args[3])
+
+	def __str__(self):
+		return "{Wall x=[" + str(self.x_start) + ", " + str(self.x_end) + "], y=[" + str(self.y_start) + ", " + str(self.y_end) + "]}"
+
+BABY = Map(msgs.BABY, "baby.map")
