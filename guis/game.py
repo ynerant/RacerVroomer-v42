8abaff931ddbcb5dc883d4a55652e2fcd1b6a779
galaxyoyo <yohann.danello@gmail.com>
2017-04-18 18:49:06 +0200

    CAR MOVING IS NOW WORKING ! FUCK YEAH !!!

--- guis/game.py
+++ guis/game.py
@@ -11,45 +11,37 @@ class Game(GUI):
 	def __init__(self, window, builder):
 		GUI.__init__(self)
 
-		window.wm_attributes("-topmost", True)
-		window.wm_attributes("-disabled", True)
-		window.wm_attributes("-transparentcolor", "purple")
-
 		self.raw_car = builder.car
 		self.map = builder.map
-		self.background = tk.PhotoImage(file = "images/maps/" + self.map.img_file)
 
-		background_label = tk.Label(window, image = self.background)
-		background_label.place(x = 0, y = 0, relwidth = 1, relheight = 1)
-		background_label.image = self.background
+		self.canvas = tk.Canvas(window, width = window.winfo_screenwidth(), height = window.winfo_screenheight(), bg = "green")
+
+		self.background_img = ImageTk.PhotoImage(file = "images/maps/" + self.map.img_file)
+		self.background = self.canvas.create_image(window.winfo_screenwidth(), window.winfo_screenheight(), image = self.background_img)
+		self.canvas.focus_set()
+		self.canvas.pack()
+		self.canvas.coords(self.background, window.winfo_screenwidth() / 2, window.winfo_screenheight() / 2)
 
 		self.car = Car(window, self)
 
-		self.children.append(background_label)
+		self.children.append(self.canvas)
 
 class Car:
 	def __init__(self, window, game):
 		self.game = game
 		self.car = game.raw_car
+		self.window = window
 
-		img_temp = Image.open("images/cars/" + self.car.img_file)
-		img_temp = img_temp.convert("RGBA")
-		img_temp = img_temp.resize((self.car.width, self.car.height), Image.ANTIALIAS)
-		img_temp.save("images/cars/tmp." + self.car.img_file)
-		img = ImageTk.PhotoImage(img_temp)
 		start_line = game.map.start
 		self.x = int((start_line.x_end + start_line.x_start) / 2) - 60
 		self.y = int((start_line.y_end + start_line.y_start) / 2) - 120
-		#self.frame = tk.Frame(window)
-		#self.canvas = tk.Canvas(self.frame, width = self.car.width, height = self.car.height)
-		#box = self.canvas.create_image(0, 0, image = img)
-		#print(box)
-		#self.frame.place(x = self.x, y = self.y)
-		#self.canvas.pack()
-		#game.children.append(self.frame)
-		self.car_label = tk.Label(window, image = img, bg = "purple")
-		self.car_label.image = img
-		self.car_label.place(x = self.x, y = self.y)
+		self.canvas = game.canvas
+		img_temp = Image.open("images/cars/" + self.car.img_file)
+		img_temp = img_temp.convert("RGBA")
+		img_temp = img_temp.resize((self.car.width, self.car.height), Image.ANTIALIAS)
+		self.img_img = ImageTk.PhotoImage(img_temp)
+		self.img = self.canvas.create_image(0, 0, image = self.img_img)
+		self.canvas.coords(self.img, self.x - self.car.width / 2, self.y - self.car.height / 2)
 
 		self.speed = 0.0
 		self.angle = 0.0
@@ -90,16 +82,18 @@ class Car:
 		img_temp = img_temp.convert("RGBA")
 		img_temp = img_temp.resize((self.car.width, self.car.height), Image.ANTIALIAS)
 		img_temp = img_temp.rotate(-self.angle * 180 / math.pi, expand = 1)
-		img_temp.save("images/cars/tmp." + self.car.img_file)
-		self.car_label.destroy()
-		self.car_label = tk.Label(self.car_label.winfo_parent(), image = ImageTk.PhotoImage(img_temp), bg = "purple")
-		self.car_label.image = ImageTk.PhotoImage(img_temp)
-		self.car_label.place(x = self.x, y = self.y)
-		#self.canvas.create_image(0, 0, image = tk.PhotoImage(file = "images/cars/tmp." + self.car.img_file))
+		self.img_img = ImageTk.PhotoImage(img_temp)
+		self.img = self.canvas.create_image(self.car.width, self.car.height, image = self.img_img)
 
 	def right(self):
 		self.angle += math.pi / 16
 		self.vector = self.angle_to_normalized_vector()
+		img_temp = Image.open("images/cars/" + self.car.img_file)
+		img_temp = img_temp.convert("RGBA")
+		img_temp = img_temp.resize((self.car.width, self.car.height), Image.ANTIALIAS)
+		img_temp = img_temp.rotate(-self.angle * 180 / math.pi, expand = 1)
+		self.img_img = ImageTk.PhotoImage(img_temp)
+		self.img = self.canvas.create_image(self.car.width, self.car.height, image = self.img_img)
 
 class CarThread(Thread):
 	def __init__(self, car):
@@ -111,6 +105,6 @@ class CarThread(Thread):
 		while True:
 			car.x += car.speed * car.vector[0] / 60.0
 			car.y += car.speed * car.vector[1] / 60.0
-			car.speed *= 0.97
-			car.car_label.place(x = int(car.x), y = int(car.y))
+			car.speed *= 0.98
+			car.canvas.coords(car.img, car.x - car.car.width / 2, car.y - car.car.height / 2)
 			time.sleep(1.0 / 60.0)
